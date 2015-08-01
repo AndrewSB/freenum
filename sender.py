66307d96719485696444897a6a2e20b6df73c214
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-01 16:48:06 -0700

    add log in send Message

--- sender.py
+++ sender.py
@@ -3,4 +3,5 @@ from flask import Flask
 
 def sendMessage(reciever, content):
     payload = {'number': reciever, 'message': content}
+    print 'Sending message to ' + str(reciever) + ' with content ' + str(content)
     return requests.post('http://textbelt.com/text', data=payload)
