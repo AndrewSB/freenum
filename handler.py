3b57a45267aec7f93ab28e62b1c9ebfd884a83ee
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-01 16:19:34 -0700

    configure the twilio handler

--- handler.py
+++ handler.py
@@ -1,1 +1,12 @@
-handle twillio response
+from flask import Flask, request, redirect
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'], defaults={'path': ''})
+@app.route('/<path:path>', methods=['POST'])
+def catch_all(path):
+    return 'You want path: %s' % path
+
+if __name__ == '__main__':
+    app.run()
