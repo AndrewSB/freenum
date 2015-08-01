833226f853c5c9be0e8aec37e4285f578fab6458
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-01 16:25:37 -0700

    create sendMessage func

--- handler.py
+++ handler.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from sender import sendMessage
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST'])
 def catch_all(path):
+    sendMessage('6508155781', str(path))
     return 'You want path: %s' % path
 
 if __name__ == '__main__':
--- sender.py
+++ sender.py
@@ -1,1 +1,6 @@
-exposes a send message func 
+import requests
+from flask import Flask
+
+def sendMessage(reciever, content):
+    payload = {'number': reciever, 'message': content}
+    return requests.post('http://textbelt.com/text', data=payload)
