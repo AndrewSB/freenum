a5bbf139ad700057ac55222b91653de95a57a573
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-01 17:18:21 -0700

    add troppo to the handler

--- handler.py
+++ handler.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from tropo import Tropo, Session
 from sender import sendMessage
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST'])
-def catch_all(path):
-    sendMessage(str(path), request.values.get('Body'))
+def handleText(path):
+    if request.values.get('Body'):
+        print "Recieved Twillio"
+        sendMessage(str(path), request.values.get('Body'))
+    else if Session(request.body).initialText:
+        print "Recieved Troppo"
+        sendMessage(str(path), str(Session(request.body).initialText))
+    else:
+        print "Recieved a message, but it seemed to be empty 😳"
+        sendMessage(str(path), "Recieved a message, but it seemed to be empty 😳")
     return 'ayy'
 
 if __name__ == '__main__':
